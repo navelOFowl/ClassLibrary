2229c0a4f494c776262a9d3c6994c593cef67116
navelOFowl <smilyck@gmail.com>
2022-04-18 11:30:31 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Tools
+    {
+        public static int Discount(int count, int totalCost)
+        {
+            float price = 0;
+            if (count >= 3 && count < 5)
+            {
+                price /= totalCost * 0.95F;
+            }
+            else if (count >= 5 && count < 10)
+            {
+                price /= totalCost * 0.90F;
+            }
+            else if (count >= 10 && count < 5)
+            {
+                price /= totalCost * 0.85F;
+            }
+            int priceWithDis = (int)Math.Floor(price);
+            return priceWithDis;
+        }
+    }
+}
